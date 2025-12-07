42ae040101473232a1aa290967ac274aef7b74f0
lado lado <roundandround2008@gmail.com>
2025-12-07 16:22:41 +0400

    Minor changes

--- Day_2/Day_2.hs
+++ Day_2/Day_2.hs
@@ -3,7 +3,7 @@
 import Data.Bifunctor (bimap)
 import Data.ByteString.Lazy (split)
 import Data.Char (digitToInt)
-import Data.List (foldl)
+import Data.List (foldl, head)
 import Data.Maybe (fromMaybe)
 import Data.Text qualified as T
 import Text.Read (Lexeme (String), readMaybe)
@@ -33,25 +33,27 @@ both :: (a -> b) -> (a, a) -> (b, b)
 both f = bimap f f
 
 -- >>> fromDigits [1,2,3]
+-- 123
 fromDigits :: [Integer] -> Integer
-fromDigits = foldl (\acc d -> acc * 10 + d) 0
-
--- >>> flattenTuple ([1], [1])
--- (1,1)
-flattenTuple :: ([a], [b]) -> (a, b)
-flattenTuple (x : _, y : _) = (x, y)
-flattenTuple _ = error "Empty list"
+fromDigits = read . concatMap show
 
 textToInteger :: T.Text -> Integer
 textToInteger t = fromMaybe (error "Invalid number format") (readMaybe (T.unpack t))
 
--- >>> parseInt "11-22"
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+  "" -> []
+  s' -> w : wordsWhen p s''
+   where
+    (w, s'') = break p s'
+
+-- >>> parseRange' "11-22"
 -- (11,22)
 parseRange :: String -> (Integer, Integer)
 parseRange s =
-  let parts = T.splitOn "-" (T.pack s)
+  let parts = wordsWhen (== '-') s
    in case parts of
-        [start, end] -> (textToInteger start, textToInteger end)
+        [start, end] -> (read start :: Integer, read end :: Integer)
         _ -> error ("Invalid range format: " ++ s)
 
 -- >>> isHalfRepeating 1212
@@ -68,15 +70,35 @@ isHalfRepeating x
  where
   ds = digits x
 
+-- >>> hasRepeating [1,2,1,2]
+-- 1212
+hasRepeating :: [Integer] -> Integer
+hasRepeating xs =
+  if any isRepeat [1 .. n `div` 2]
+    then fromDigits xs
+    else 0
+ where
+  n = length xs
+
+  isRepeat k =
+    let a = take k xs
+     in n `mod` k == 0
+          && concat (replicate (n `div` k) a) == xs
+
 -- >>> makeRange (11,33)
 -- [11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
 makeRange :: (Integer, Integer) -> [Integer]
 makeRange (x, y) = [x .. y]
 
--- >>> solution "1111-1130"
--- 1111
+-- >>> solution "2121212118-2121212124"
+-- 0
 solution :: String -> Integer
-solution a = sum $ map isHalfRepeating $ makeRange (parseRange a)
+solution = sum . map isHalfRepeating . makeRange . parseRange
+
+-- >>> solution' "2121212118-2121212124"
+-- 2121212121
+solution' :: String -> Integer
+solution' = sum . map (hasRepeating . digits) . makeRange . parseRange
 
 main = do
   contents <- readFile "Day_2\\input.txt"
